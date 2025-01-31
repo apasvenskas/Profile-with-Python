1b15cb9481f1e8fed976fed1c7b9651e10f4f7f6
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-01-30 21:20:38 -0800

    render python project titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +14,19 @@ with col2:
         Hello, my name is Audrius, and I am a programmer who primarily works with JavaScript, Python, and C++. My journey into programming has been unconventional. I started with self-learning, attended a coding bootcamp, and am currently advancing my programming knowledge through various projects and college studies. I plan to further solidify my programming skills by earning a degree from a university. Unfortunately, the current job market has not been favorable to me, and my professional experience is somewhat limited to personal and voluntary projects.
      """
     st.info(content)
+
+content2 = """
+    Here is a library of apps that I have built using Python.
+    """
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep = ";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[:10].iterrows(): # [:10] upt ot ten
+        st.header(row["title"])
